4481792e30f8b464522ac1357b3fca214e5a625c
Damián <damian2gom@gmail.com>
2018-03-16 10:52:00 -0600

    Input:disable y radio (css)

--- views/localidades/_form.php
+++ views/localidades/_form.php
@@ -116,14 +116,15 @@ $porcentajeAbogado = CatPorcentajeRentaAbogados::find()->where(['id_usuario'=>$i
             
             <div class="col-sm-12 col-md-12 col-lg-4">
 
-                <p>
+                <div class="radio-custom radio-warning">
                     <input name="group1" type="radio" id="test-regularizacion-<?=$idUser?>" value="regularizacion" onclick="statusLocalidad($(this));" checked="checked" /> 
                     <label for="test-regularizacion-<?=$idUser?>">Regularizacion</label>
-                </p>
-                <p>
-                    <input name="group1" type="radio" id="test-renovacion-<?=$idUser?>" value="renovacion" onclick="statusLocalidad($(this));" /> 
+                </div>
+
+                <div class="radio-custom radio-warning">
+                    <input name="group1" type="radio" id="test-renovacion-<?=$idUser?>" value="renovacion" onclick="statusLocalidad($(this));" />  
                     <label for="test-renovacion-<?=$idUser?>">Renovacion</label>
-                </p>
+                </div>
 
                 <?= $form->field($model, 'b_status_localidad')->hiddenInput()->label(false) ?>
 
